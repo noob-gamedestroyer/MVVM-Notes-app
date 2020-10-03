e74356993c0dcbe0e2b4e424fb693b388ecdd515
noob-gamedestroyer <60071765+noob-gamedestroyer@users.noreply.github.com>
2020-10-03 18:15:10 +0530

    added splash screen

---
+++ app/src/main/java/com/gamdestroyerr/roomnote/ui/activity/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.gamdestroyerr.roomnote.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this@SplashActivity, NoteActivity::class.java)
+        startActivity(intent)
+        finish()
+
+    }
+
+}
